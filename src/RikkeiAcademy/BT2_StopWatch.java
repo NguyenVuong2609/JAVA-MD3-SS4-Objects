024e1244a284493543544d65d49ee6dd3898d7e7
Nguyen Vuong 2609 <ducvuong.269@gmail.com>
2023-03-03 14:37:34 +0700

    0303

--- src/RikkeiAcademy/BT2_StopWatch.java
+++ src/RikkeiAcademy/BT2_StopWatch.java
@@ -1,16 +1,51 @@
 package RikkeiAcademy;
 
 import java.util.Date;
+import java.util.Scanner;
 
 public class BT2_StopWatch {
     public static void main(String[] args) {
+        Date recent = new Date();
+        Scanner sc = new Scanner(System.in);
         StopWatch watch = new StopWatch();
+        int choice = -1;
+        while (choice != 0) {
+            System.out.println("Menu");
+            System.out.println("1. Reset the time");
+            System.out.println("2. Stop the time");
+            System.out.println("3. Show the distance");
+            System.out.println("0. Exit");
+            System.out.println("Enter your choice:");
+            choice = sc.nextInt();
+            switch (choice){
+                case 1:
+                    watch.start();
+                    System.out.println("Now: " + watch.startTime);
+                    break;
+                case 2:
+                    watch.stop(recent.getTime());
+                    System.out.println("End: " + watch.endTime);
+                    break;
+                case 3:
+                    if (watch.endTime == 0){
+                        System.out.println("Please stop the time.");
+                    } else {
+                        System.out.println("Distance: " + watch.getElapsedTime());
+                    }
+                    break;
+                case 0:
+                    System.exit(0);
+                default:
+                    System.out.println("No choice.");
+            }
+        }
+
     }
 
-    public static class StopWatch{
+    public static class StopWatch {
         Date recentTime = new Date();
         private long startTime = recentTime.getTime();
-        private long endTime;
+        private long endTime = 0;
 
         public StopWatch() {
         }
@@ -31,13 +66,15 @@ public class BT2_StopWatch {
             this.endTime = endTime;
         }
 
-        public void start(){
+        public void start() {
             startTime = recentTime.getTime();
         }
-        public void end(){
-            endTime = recentTime.getTime();
+
+        public void stop(long newTime) {
+            endTime = newTime;
         }
-        public long getElapsedTime(){
+
+        public long getElapsedTime() {
             long distance = endTime - startTime;
             return distance;
         }
